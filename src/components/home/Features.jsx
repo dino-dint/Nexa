a2ace40e39a1d546f21a5ccb82a78424da9e1ded
dino-dint <rimurusimping@gmail.com>
2026-08-25 23:07:16 +0700

    add the Content into Feature.jsx for Homepage

--- src/components/home/Features.jsx
+++ src/components/home/Features.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function Features() {
   return (
-    <div>
-      
+    <div >
+      {/* <div className='h-125 flex justify-center items-start'>
+        <h1 className='text-4xl md:text-6xl text-center text-white font-bold pt-3'>
+          Feature Product
+        </h1>
+      </div> */}
     </div>
   )
 }
