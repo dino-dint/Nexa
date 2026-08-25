da8ba5a6cc49574f4f8d4d84a84b31dd24859210
dino-dint <rimurusimping@gmail.com>
2026-08-25 18:13:04 +0700

    change small component for both Laptop and Mobile

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -23,16 +23,11 @@ function Navbar() {
   };
 
   return (
-    <div>
-      <nav className="bg-[rgb(223, 222, 222)] text-white px-3 py-3 relative z-40">
-        <div className="flex justify-between items-center bg-zinc-950 h-14 px-3 rounded-3xl shadow-2xl">
+    <div className="fixed top-0 left-0 w-full z-50 bg-black">
+      <nav className=" text-white px-3 py-3 relative z-40">
+        <div className="flex justify-between items-center bg-black h-14 px-3 rounded-3xl shadow-2xl">
           {/* 1. LEFT: Logo */}
           <div className="shop-logo text-xl font-bold text-white flex items-center gap-3">
-            <img
-              src={nexaLogo}
-              alt="Nexa-logo"
-              className="w-10 h-10 object-contain rounded-xl"
-            />{" "}
             <button
               className="hover:text-slate-500 cursor-pointer"
               onClick={() => handleNavClick("Home")}
@@ -102,7 +97,10 @@ function Navbar() {
                 <button
                   key={item}
                   type="button"
-                  onClick={() => handleNavClick(item)}
+                  onClick={() => {
+                    handleNavClick(item);
+                    setProductsOpen(false);
+                  }}
                   className={`relative py-1 transition-colors duration-300 cursor-pointer ${
                     active
                       ? "text-white font-medium"
@@ -122,19 +120,19 @@ function Navbar() {
           </div>
 
           <div className="flex items-center space-x-4">
-              <button className="hidden md:block p-2 text-gray-300  hover:text-white rounded-full hover:bg-zinc-800 transition-colors">
+              <button className="hidden md:block p-2 text-gray-300  hover:text-white rounded-full hover:bg-zinc-800 transition-colors cursor-pointer">
                 <ShoppingCart size={22} />
               </button>
-              <button type="button" className="hidden md:block p-2 text-zinc-800  rounded-full bg-cyan-300  hover:bg-cyan-200 transition-colors ">
+              <button type="button" className="hidden md:block p-2 text-zinc-800  rounded-full bg-cyan-300  hover:bg-cyan-200 transition-colors cursor-pointer">
                 <UserRound size={22}/>
               </button>
             </div>
 
-          {/* 3. RIGHT: Cart + Mobile Menu Button Grouped Together */}
+          {/* 3. RIGHT: Menu Button for Mobile */}
           <div className="flex items-center space-x-2">
             <button
               type="button"
-              className="block md:hidden p-2 text-gray-700 hover:text-black focus:outline-none relative z-50"
+              className="block md:hidden p-2 text-white hover:text-slate-400 focus:outline-none relative z-50"
               onClick={() => setIsOpen(true)}
             >
               <Menu size={26} />
@@ -145,14 +143,14 @@ function Navbar() {
         {/* Mobile Dropdown */}
         <div
           // this is for mobile
-          className={`fixed top-0 right-0 h-full w-64 bg-slate-100 shadow-2xl p-6 transition-transform duration-300 ease-in-out md:hidden z-50 ${
+          className={`fixed top-0 right-0 h-full w-64 bg-black shadow-2xl p-6 transition-transform duration-300 ease-in-out md:hidden z-50 ${
             isOpen ? "translate-x-0" : "translate-x-full"
           }`}
         >
           <div className="flex justify-end mb-6">
             <button
               onClick={() => setIsOpen(false)}
-              className="p-2 text-gray-700 hover:text-black"
+              className="p-2 text-white hover:text-slate-400"
             >
               <X size={26} />
             </button>
@@ -161,25 +159,25 @@ function Navbar() {
           <div className="flex flex-col space-y-3">
             <a
               href="#"
-              className="py-2 flex gap-4 text-gray-700 hover:text-zinc-900 rounded px-2"
+              className="py-2 flex gap-4 text-white hover:text-slate-400 rounded px-2"
             >
               <Home size={22} className="home-logo" /> Home
             </a>
             <a
               href="#"
-              className="flex gap-4 py-2 text-gray-700 hover:text-zinc-900 rounded px-2"
+              className="flex gap-4 py-2 text-white hover:text-slate-400 rounded px-2"
             >
               <ShoppingBasket size={22} className="shopping-logo" /> Products
             </a>
             <a
               href="#"
-              className="flex gap-4 py-2 text-gray-700 hover:text-zinc-900 rounded px-2"
+              className="flex gap-4 py-2 text-white hover:text-slate-400 rounded px-2"
             >
               <ListSortAscending size={22} className="category" /> Categories
             </a>
             <a
               href="#"
-              className="flex gap-4 py-2 text-gray-700 hover:text-zinc-900 rounded px-2"
+              className="flex gap-4 py-2 text-white hover:text-slate-400 rounded px-2"
             >
               <UserRound size={22} className="profile" /> Profile
             </a>
