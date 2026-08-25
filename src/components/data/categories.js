55122adf74549dd73a63e220f455b97e49ea54d4
dino-dint <rimurusimping@gmail.com>
2026-08-25 23:03:37 +0700

    add static data into categories.js

---
+++ src/components/data/categories.js
@@ -0,0 +1,27 @@
+const categories = [
+    {
+        id: 1,
+        name: "Laptops",
+        slug: "laptops",
+        image: "/public/categories/laptop.jpg"
+    },
+    {
+        id: 2,
+        name: "Smartphones",
+        slug: "smartphones",
+        image: "/public/categories/smartphone.jpg"
+    },
+    {
+        id: 3,
+        name: "Headphones",
+        slug: "headphones",
+        image: "/public/categories/headphone.jpg"
+    },
+    {
+        id: 4,
+        name: "Accessories",
+        slug: "accessories",
+        image: "/public/categories/accessories.jpg"
+    }
+]
+export default categories;
