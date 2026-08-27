f3026d701bc382868a4c7365412d0f97877640c6
dino-dint <rimurusimping@gmail.com>
2026-08-27 23:46:01 +0700

    add product details as array in featureProducts.js for Homepage

---
+++ src/components/data/featureProducts.js
@@ -0,0 +1,45 @@
+
+const features = [
+    {
+        id: 1,
+        title: 'Best Sellers',
+        category: 'Laptop',
+        tag: 'Top Rated',
+        name: 'ASUS ROG Zephyrus G16',
+        image:'/features/laptop.jpg',
+        price: "2,399"
+    },
+
+    {
+        id: 2,
+        title: 'Flash Sale',
+        category: 'Smartphone',
+        tag: 'Limited Time',
+        name: 'Samsung Galaxy S26 Ultra',
+        image: '/features/smartphone.jpg',
+        price: "1,299"
+
+    },
+    {
+        id: 3,
+        title: 'New Arrivals',
+        category: 'Headphone/IEM',
+        tag: 'Fresh Drops',
+        name: 'Airpod Max',
+        image: '/features/headphone.jpg',
+        price: "549"
+
+    },
+    {
+        id: 4,
+        title: 'Trending',
+        category: 'Accessories',
+        tag: 'Hot Now',
+        name: 'FIFINE AmpliTank K688',
+        image: '/features/accessories.jpg',
+        price: '73'
+
+    },
+]
+
+export default features;
