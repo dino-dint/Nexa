9966e3c88e698cbf29cc68be0b91041b79ece96d
dino-dint <rimurusimping@gmail.com>
2026-08-27 23:38:22 +0700

    make small changes in categories.js

--- src/components/data/categories.js
+++ src/components/data/categories.js
@@ -3,25 +3,25 @@ const categories = [
         id: 1,
         name: "Laptops",
         slug: "laptops",
-        image: "/public/categories/laptop.jpg"
+        image: "/categories/laptop.jpg"
     },
     {
         id: 2,
         name: "Smartphones",
         slug: "smartphones",
-        image: "/public/categories/smartphone.jpg"
+        image: "/categories/smartphone.jpg"
     },
     {
         id: 3,
         name: "Headphones",
         slug: "headphones",
-        image: "/public/categories/headphone.jpg"
+        image: "categories/headphone.jpg"
     },
     {
         id: 4,
         name: "Accessories",
         slug: "accessories",
-        image: "/public/categories/accessories.jpg"
+        image: "categories/accessories.jpg"
     }
 ]
 export default categories;
